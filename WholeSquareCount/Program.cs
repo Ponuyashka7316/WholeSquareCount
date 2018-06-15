25e978e0917b8616ba4c9dd754990a22357c1566
wtwet <yuriygurin1@mail.ru>
2018-06-15 15:17:37 +0300

    new class

--- WholeSquareCount/Program.cs
+++ WholeSquareCount/Program.cs
@@ -7,20 +7,7 @@ using System.Threading.Tasks;
 namespace WholeSquareCount
 {
 
-    class Solution
-    {
-        public int WholeSquareCount(int A, int B)
-        {
-            int count = 0;
-            for (int i = 0; i<=Int32.MaxValue; i++)
-            {
-                if (Enumerable.Range(A, B).Contains((int)Math.Pow(i, 2)))
-                    count++ ;
-               else  if (Math.Pow(i, 2) > B) return count;
-            }
-            return count;
-        }
-    }
+   
 
     class Program
     {
---
+++ WholeSquareCount/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WholeSquareCount
+{
+    class Solution
+    {
+        public int WholeSquareCount(int A, int B)
+        {
+            int count = 0;
+            for (int i = 0; i <= Int32.MaxValue; i++)
+            {
+                if (Enumerable.Range(A, B).Contains((int)Math.Pow(i, 2)))
+                    count++;
+                else if (Math.Pow(i, 2) > B) return count;
+            }
+            return count;
+        }
+    }
+}
