0826e5770bfd176590b04d37ccc746421748c3f9
wtwet <yuriygurin1@mail.ru>
2018-06-15 15:16:10 +0300

    Initial commit

--- WholeSquareCount/Program.cs
+++ WholeSquareCount/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace WholeSquareCount
 {
+
+    class Solution
+    {
+        public int WholeSquareCount(int A, int B)
+        {
+            int count = 0;
+            for (int i = 0; i<=Int32.MaxValue; i++)
+            {
+                if (Enumerable.Range(A, B).Contains((int)Math.Pow(i, 2)))
+                    count++ ;
+               else  if (Math.Pow(i, 2) > B) return count;
+            }
+            return count;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            int A = 4;
+            int B = 17;
+            Solution sln = new Solution();
+            Console.WriteLine("Answer: "+sln.WholeSquareCount(A,B));
+            Console.ReadKey();
+
         }
     }
 }
